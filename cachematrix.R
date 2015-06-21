22016678ad8d3bc556cd39f10995dac8c987cbba
viktorkomsalov <viktor.komsalov@gmail.com>
2015-06-21 14:03:16 -0400

    Assignment 2 the two functions
    Here are two functions one that sets the list with the matrix and its
    cache inverse, and the second that checks if the cache exists and if not
    computes and sets it.
    
    Also, comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This set of functions is meant to compute the inverse of a matrix
+## and reuse it from the cache if it already been computed to save time.
 
-## Write a short comment describing this function
+## The first function creates a saves the matrix in multiple
+## environments and creates a list for the inverse that can be
+## reused instead of being computed if the same matrix' inverse
+## is requested again.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	I <- NULL
+	set <- function(y) {
+		x <<- y
+		I <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(inverse) I <<- inverse
+	getinverse <- function() I
+	list(set = set,
+		 get = get,
+		 setinverse = setinverse,
+		 getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The second function checks if the inverse has been computed,
+## and if not computes the inverse and then sets it in the 
+## makeCacheMatrix list for further use.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+		I <- x$getinverse()
+		if(!is.null(I)) {                      ##Check if inverse is in cache
+				message("getting cached data")
+				return(I)
+		}
+		data <- x$get()
+		I <- solve(data, ...)                 ##If not compute and return inverse
+		x$setinverse(I)
+		I
 }
+
+
